822ffdb2e843b47ad28bb53f3f2281ff4004d414
Ajay Videkar <avidekar@gmail.com>
2020-07-27 14:01:30 -0700

    Add files via upload

---
+++ package2/TC_PaymentReturnsTest.py
@@ -0,0 +1,9 @@
+import unittest
+
+class PaymentReturnsTest(unittest.TestCase):
+    def test_payment_return_by_bank(self):
+        print("This is payment return by bank")
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
