3efed08a9d3de771e1e703c7b8cb79d67b0e2202
Ajay Videkar <avidekar@gmail.com>
2020-07-27 14:00:25 -0700

    Add files via upload

---
+++ package1/TC_SignUpTest.py
@@ -0,0 +1,18 @@
+import unittest
+
+class SignUpTest(unittest.TestCase):
+
+    def test_signup_by_mail(self):
+        print("This is signup by email test")
+        self.assertTrue(True)
+
+    def test_signup_by_facebook(self):
+        print("This is signup by facebook test")
+        self.assertTrue(True)
+
+    def test_signup_by_twitter(self):
+        print("This is signup by twitter test")
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
